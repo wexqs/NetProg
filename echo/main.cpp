6d7890e9f6d874ab85ee2506a358d2f8bbd93ea9
wexqs <pufik2901@mail.ru>
2024-10-15 21:47:58 +0300

    Update main.cpp

--- echo/main.cpp
+++ echo/main.cpp
@@ -1,1 +1,62 @@
+#include <iostream>
+#include <sys/types.h>
+#include <unistd.h>
+#include <sys/socket.h>
+#include <netdb.h>
+#include <arpa/inet.h>
+#include <string.h>
+#include <string>
 
+using namespace std;
+
+int main() // TCP
+{
+    int sock = socket(AF_INET, SOCK_STREAM, 0);
+    if (sock == -1)
+    {
+        return 1;
+    }
+
+    int port = 44214;
+    string ipAddress = "172.16.40.1";
+    sockaddr_in hint;
+    hint.sin_family = AF_INET;
+    hint.sin_port = htons(port);
+    inet_pton(AF_INET, ipAddress.c_str(), &hint.sin_addr);
+
+    int connectRes = connect(sock, (sockaddr*)&hint, sizeof(hint));
+    if (connectRes == -1)
+    {
+        cout << "Error connection" << endl;
+        return 1;
+    }
+
+    char buf[4096];
+    string userInput;
+    
+    do {
+        cout << "n> ";
+        getline(cin, userInput);
+
+        int sendRes = send(sock, userInput.c_str(), userInput.size() + 1, 0);
+        if (sendRes == -1)
+        {
+            cout << "Could not send to server! Whoops!rn";
+            continue;
+        }
+
+        memset(buf, 0, 4096);
+        int bytesReceived = recv(sock, buf, 4096, 0);
+        if (bytesReceived == -1)
+        {
+            cout << "There was an error getting response from serverrn";
+        }
+        else
+        {
+            cout << "nSERVER> " << string(buf, bytesReceived) << "rn";
+        }
+    } while (true);
+
+    close(sock);
+    return 0;
+}
