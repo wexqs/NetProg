1fadf3d53537b18e03bacdcf2aed09ac78c002a0
wexqs <pufik2901@mail.ru>
2024-11-11 19:33:02 +0300

    Update main.cpp

--- echo/main.cpp
+++ echo/main.cpp
@@ -1,61 +1,57 @@
 #include <iostream>
-#include <sys/types.h>
-#include <unistd.h>
+#include <cstring>
 #include <sys/socket.h>
-#include <netdb.h>
-#include <arpa/inet.h>
-#include <string.h>
-#include <string>
+#include <netinet/in.h>
+#include <arpa/inet.h> 
+#include <unistd.h>
 
-using namespace std;
+#define PORT 7
 
-int main() // TCP
-{
-    int sock = socket(AF_INET, SOCK_STREAM, 0);
-    if (sock == -1)
-    {
-        return 1;
+int main() {
+    int sock = 0;
+    struct sockaddr_in serv_addr;
+    char buffer[1024];
+
+    if ((sock = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
+        std::cerr << "Ошибка создания сокета" << std::endl;
+        return -1;
     }
 
-    int port = 7;
-    string ipAddress = "172.16.40.1";
-    sockaddr_in hint;
-    hint.sin_family = AF_INET;
-    hint.sin_port = htons(port);
-    inet_pton(AF_INET, ipAddress.c_str(), &hint.sin_addr);
+    serv_addr.sin_family = AF_INET;
+    serv_addr.sin_port = htons(PORT);
 
-    int connectRes = connect(sock, (sockaddr*)&hint, sizeof(hint));
-    if (connectRes == -1)
-    {
-        cout << "Error connection" << endl;
-        return 1;
+    if (inet_pton(AF_INET, "172.16.40.1", &serv_addr.sin_addr) <= 0) {
+        std::cerr << "Неверный адрес" << std::endl;
+        return -1;
     }
 
-    char buf[4096];
-    string userInput;
-    
-    do {
-        cout << "n> ";
-        getline(cin, userInput);
+    if (connect(sock, (struct sockaddr *)&serv_addr, sizeof(serv_addr)) < 0) {
+        std::cerr << "Ошибка подключения" << std::endl;
+        return -1;
+    }
 
-        int sendRes = send(sock, userInput.c_str(), userInput.size() + 1, 0);
-        if (sendRes == -1)
-        {
-            cout << "Could not send to server! Whoops!rn";
-            continue;
+    while (true) {
+        std::string message;
+        std::cout << "Введите сообщение для отправки (или 'exit' для выхода): ";
+        std::getline(std::cin, message);
+        
+        if (message == "exit") break;
+
+        ssize_t sent_bytes = send(sock, message.c_str(), message.size(), 0);
+        if (sent_bytes < 0) {
+            std::cerr << "Ошибка отправки сообщения" << std::endl;
+            break;
         }
 
-        memset(buf, 0, 4096);
-        int bytesReceived = recv(sock, buf, 4096, 0);
-        if (bytesReceived == -1)
-        {
-            cout << "There was an error getting response from serverrn";
+        memset(buffer,'\0', sizeof(buffer));
+        int valread = read(sock, buffer, sizeof(buffer) - 1); 
+        if (valread < 0) {
+            std::cerr << "Ошибка чтения ответа от сервера" << std::endl;
+            break;
         }
-        else
-        {
-            cout << "nSERVER> " << string(buf, bytesReceived) << "rn";
-        }
-    } while (true);
+
+        std::cout << "Получено от сервера: " << std::string(buffer, valread) << std::endl;
+    }
 
     close(sock);
     return 0;
